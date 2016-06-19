6399ce2056f8acaf2a2b27b2321e6d72f2db6ac3
Peter Cho <pcho@phase2technology.com>
2016-06-19 02:49:04 -0400

    Refactored to instantiate callbacks

--- controllers/wmata.js
+++ controllers/wmata.js
@@ -6,13 +6,24 @@ const util = require('../util/util.js');
 
 module.exports = {
   // Acquire metadata of all rail stations from WMATA API.
-  set_metadata: (db) => {
+  set_metadata: (db, callback) => {
     let parameters = {
       url: 'https://api.wmata.com/Rail.svc/json/jStations?api_key=' + process.env.WMATA_PRIMARY_KEY
     };
-
     request(parameters, function (error, response) {
       db.redis.set('wmata_metadata', response.body);
+
+      callback();
+    });
+  },
+  get_stations: (db, code, callback) => {
+    let parameters = {
+      url: 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=C15&ToStationCode=E06&api_key=' + process.env.WMATA_PRIMARY_KEY
+    };
+    request(parameters, function (error, response) {
+      db.redis.set('wmata_line_' + code, response.body);
+
+      callback();
     });
   }
 }
--- routes.js
+++ routes.js
@@ -10,9 +10,22 @@ module.exports = (db) => {
 
   // Homepage
   router.get('/', function (req, res) {
-    wmata.set_metadata(db);
-    db.redis.get('wmata_metadata', function(err, reply) {
-      res.status(200).json(JSON.parse(reply));
+    wmata.set_metadata(db, () =>{
+      db.redis.get('wmata_metadata', function(err, reply) {
+        res.status(200).json(JSON.parse(reply));
+      });
+    });
+  });
+
+  // Track individual line
+  router.get('/line/:code', function (req, res) {
+    wmata.get_stations(db, req.params.code, () => {
+      if (req.params.code.length > 2) {
+        res.send(404).done();
+      }
+      db.redis.get('wmata_line_' + req.params.code, function(err, reply) {
+        res.status(200).json(JSON.parse(reply));
+      });
     });
   });
 

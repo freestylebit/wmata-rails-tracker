80fd5d98938bfd40601e100c5cbe9f5a247bb820
Peter Cho <pcho@phase2technology.com>
2016-06-20 00:41:20 -0400

    Added two new endpoints

--- src/dataRoutes.js
+++ src/dataRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const React = require('react');
 const ReactDOM = require('react-dom/server');
+const _ = require('lodash');
 
 module.exports = (db) => {
   const router = express.Router();
@@ -14,6 +15,37 @@ module.exports = (db) => {
 
   // Track individual line
   router.get('/line/:code', (req, res) => {
+    if (req.params.code.length > 2) {
+      res.status(404).send('Page not found');
+      return;
+    }
+    db.redis.get(`wmata_line_${req.params.code}`, (err, reply) => {
+      res.status(200).json(JSON.parse(reply));
+    });
+  });
+
+  // Yields all real-time data
+  router.get('/realtime', (req, res) => {
+    // TODO: Move this to the CRON job and create a cache entry in redis
+    // so we're not regenerating this dataset every time we ping the server.
+    db.redis.get('wmata_realtime_status', (err, reply) => {
+      // Organize the real time data into a format we can use.
+      const payload = JSON.parse(reply);
+      let statuses = {};
+
+      _.map(payload.Trains, (data) => {
+        if (!statuses[data.LocationCode]) {
+          // Initialize array for data entry.
+          statuses[data.LocationCode] = [];
+        }
+        statuses[data.LocationCode].push(data.Min);
+      });
+      res.status(200).json(statuses);
+    });
+  });
+
+  // Parsed data endpoint
+  router.get('/track/:code', (req, res) => {
     if (req.params.code.length > 2) {
       res.status(404).send('Page not found');
       return;

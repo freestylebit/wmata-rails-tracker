2a1031d417e4a670da284222d52e94f92e8d1889
Peter Cho <pcho231@gmail.com>
2016-08-15 03:21:18 -0400

    Fix up wmata controller tests

--- src/spec/controllers/wmata-spec.js
+++ src/spec/controllers/wmata-spec.js
@@ -12,19 +12,19 @@ const wmata = require('../../controllers/wmata.js')(db);
 describe('WMATA Controllers - ', () => {
   it('Method get_metadata() should acquire rail metadata.', (done) => {
     wmata.get_metadata(() => {
-      let value = fakeRedis.get('wmata_metadata');
+      let value = db.redis.get('wmata_metadata');
       done();
     });
   });
   it('Method get_stations_list() should acquire rail metadata.', (done) => {
     wmata.get_stations_list('RD', () => {
-      let value = fakeRedis.get('wmata_line_RD');
+      let value = db.redis.get('wmata_line_RD');
       done();
     });
   });
   it('Method get_stations_status() should acquire rail metadata.', (done) => {
     wmata.get_stations_status(() => {
-      let value = fakeRedis.get('wmata_metadata');
+      let value = db.redis.get('wmata_realtime_status');
       done();
     });
   });

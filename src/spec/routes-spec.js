b363138a1d93b4f86743285381b772c7fb56fa49
Peter Cho <pcho231@gmail.com>
2016-08-14 21:29:46 -0400

    Fixed data route test

--- src/spec/routes-spec.js
+++ src/spec/routes-spec.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const request = require('supertest');
 const chai = require('chai');
+const bodyParser = require('body-parser');
 const redisMock = require('redis-mock');
 
 // Create mock of redis database layer for testing purposes.
@@ -13,16 +14,19 @@ const db = {
 chai.should();
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+require('dotenv').config();
+require('node-jsx').install();
 
 // Set views/ directory especially for the following test suites.
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'ejs');
 
-
 describe('Application routes', () => {
   before(() => {
-    const routes = require('../routes.js')(db);
-    app.use('/', routes);
+    app.use('/', require('../routes.js')(db));
   });
   it('should detect a valid page at site root.', (done) => {
     request(app)
@@ -36,15 +40,16 @@ describe('Application routes', () => {
   });
 });
 
+
+
 describe('Data routes', () => {
   before(() => {
-    const dataRoutes = require('../dataRoutes.js')(db);
-    app.use('/v1/data', dataRoutes);
-  });
+    app.use('/v1/data', require('../dataRoutes.js')(db));
+  })
   it('should detect a valid JSON endpoint (metadata) at /v1/data/.', (done) => {
     request(app)
-      .get('/')
+      .get('/v1/data')
       .expect('Content-Type', /json/)
-      .expect(200, done);
+      .expect(400, {"error":"NO DATA"}, done);
   });
 });

5d5af992c0a1fd9992f074767a7bcfcc8c434394
Peter Cho <pcho231@gmail.com>
2016-08-15 04:46:52 -0400

    Truly fixed wmata controller tests

--- spec/controllers/wmata-spec.js
+++ spec/controllers/wmata-spec.js
@@ -12,20 +12,27 @@ const wmata = require('../../src/controllers/wmata.js')(db);
 describe('WMATA Controllers - ', () => {
   it('Method get_metadata() should acquire rail metadata.', (done) => {
     wmata.get_metadata(() => {
-      let value = db.redis.get('wmata_metadata');
-      done();
+      db.redis.get(`wmata_metadata`, (err, reply) => {
+        expect(reply).to.match(/StationTogether1/);
+        done();
+      });
     });
   });
   it('Method get_stations_list() should acquire rail metadata.', (done) => {
     wmata.get_stations_list('RD', () => {
-      let value = db.redis.get('wmata_line_RD');
-      done();
+      db.redis.get(`wmata_line_RD`, (err, reply) => {
+        expect(reply).to.match(/DistanceToPrev/);
+        done();
+      });
     });
   });
   it('Method get_stations_status() should acquire rail metadata.', (done) => {
     wmata.get_stations_status(() => {
-      let value = db.redis.get('wmata_realtime_status');
-      done();
+      db.redis.get(`wmata_realtime_status`, (err, reply) => {
+        expect(reply).to.match(/DestinationName/);
+        expect(reply).to.match(/Min/);
+        done();
+      });
     });
   });
 });
